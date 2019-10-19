b76200986f671f95086cd205f006d8aa26523731
Tim Gallo <tim@parsed.io>
2019-10-19 13:38:27 -0500

    rearrange quiz and home to avoid eslint error

--- src/home/home.container.js
+++ src/home/home.container.js
@@ -2,7 +2,9 @@ import { connect } from 'react-redux';
 import HomeComponent from './home.component';
 import { getQuestions } from './home.actions';
 
-export const Home = connect(
+const Home = connect(
   undefined,
   { getQuestions },
 )(HomeComponent);
+
+export default Home;
--- src/quiz/quiz.container.js
+++ src/quiz/quiz.container.js
@@ -2,11 +2,11 @@ import { connect } from 'react-redux';
 import QuizComponent from './quiz.component';
 import { updateQuestion } from './quiz.actions';
 
-
 const mapStateToProps = (state) => state.quizReducer;
 
-
-export const Quiz = connect(
+const Quiz = connect(
   mapStateToProps,
   { updateQuestion },
 )(QuizComponent);
+
+export default Quiz;
